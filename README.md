16056fe2c6eaaf8c5fdd557821ce2de0420f096a
Sudipto Das <dev.sudipto.das@gmail.com>
2020-02-29 01:53:22 +0600

    Phase 1: Parsing Movie Data     * Wrote functionalites for parsing movie data from provided link

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# cefalo-assignment
+# Python Developer Assignment
+### System Info
+Python verison : 3.7.4
+
+PIP version : 20.0.2
+***
+### Instruction to run the program
+
+Install requirements
+
+```python
+pip install -r requirements.txt
+```
+Start parsing
+```python
+python parse.py
+```
---
+++ parse.py
@@ -0,0 +1,99 @@
+import json
+import requests
+from urllib.parse import urljoin
+from bs4 import BeautifulSoup
+
+start_url = "https://en.wikipedia.org/wiki/List_of_Academy_Award-winning_films"
+
+
+def soup_sanitizing(soup):
+    """
+    Text formatting and sanitizing
+    """
+    for sup in soup.findAll('sup'):
+        sup.decompose()
+    
+    return soup
+
+
+def get_soup(url):
+    """
+    Parses URL and fetches movie wiki link
+    url(string): URL to parse
+    return BeautifulSoup soup
+    """
+    
+    headers = {'User-Agent' : 'Mozilla/5.0'}
+    
+    try:
+        r = requests.get(url, headers = headers, timeout=5)
+
+        if r.status_code != 200:
+            raise Exception('Unreachable URL')
+
+        return BeautifulSoup(r.content, 'html.parser')
+    except Exception:
+        return None    
+
+def get_movie_detail(url):
+    """
+    Parses URL and fetches movie info
+    url(string): URL to parse
+    return Dict 
+    
+    """
+    soup = get_soup(url)
+    data = {}
+    
+    if not soup:
+        return data
+    
+    infobox = soup.find('table', class_='infobox')
+    
+    if not infobox:
+        return data
+    
+    for tr in soup.find('table', class_='infobox').findAll('tr'):
+        th = tr.find('th')
+        td = tr.find('td')
+
+        if th and td:
+            td = soup_sanitizing(td)
+            info = td.text.strip()
+            info = info.strip('\n')
+            data[th.text] = info
+                
+    return data
+
+# Get start_url html content as soup
+soup = get_soup(start_url)
+movie_data = {}
+
+if soup:
+    # For each movie info row, fetch data
+    for tr in soup.find('table', class_='wikitable').find_all('tr'):
+        try:
+            link = urljoin(start_url, tr.find_all('td')[0].find('a').get('href'))
+            name = tr.find_all('td')[0].text.strip()
+            year = tr.find_all('td')[1].text.strip()
+            awards = soup_sanitizing(tr.find_all('td')[2]).text.strip()
+            nominations = soup_sanitizing(tr.find_all('td')[3]).text.strip()
+
+            data = {
+                'name': name,
+                'year': year,
+                'awards': awards,
+                'nominations': nominations
+            }
+
+            # Fetch movie detail data from respective movie link
+            detail = get_movie_detail(link)
+
+            # Storing movie data
+            movie_data = {**movie_data, **{ link: { **data, **detail } }}
+            
+            # Output to terminal for each parsing
+            print("Parsing %s" % link, '\n')
+            print(json.dumps({ **data, **detail }, indent=2), '\n\n')
+        except Exception:
+            pass
