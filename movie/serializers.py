e150825a8127dd2f7445f1e22d921c32c3ebf849
Sudipto Das <dev.sudipto.das@gmail.com>
2020-02-29 23:14:19 +0600

    Hotfix: Updated list and retrive methods for better representation.

--- movie/serializers.py
+++ movie/serializers.py
@@ -4,6 +4,17 @@ from rest_framework import serializers
 from movie.models import Movie
 
 
+class MovieSerializerFormattedInfo(serializers.ModelSerializer):
+    info = serializers.SerializerMethodField()
+
+    def get_info(self, obj):
+        return json.loads(obj.info)
+
+    class Meta:
+        model = Movie
+        fields = ('title', 'year', 'awards', 'nominations', 'average_rating', 'rating_givers', 'info')
+
+
 class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
--- movie/views.py
+++ movie/views.py
@@ -1,9 +1,20 @@
 from rest_framework import viewsets
+from rest_framework.response import Response
 
 from movie.models import Movie
-from movie.serializers import MovieSerializer
+from movie.serializers import MovieSerializer, MovieSerializerFormattedInfo
 
 
 class MovieViewSet(viewsets.ModelViewSet):
     serializer_class = MovieSerializer
     queryset = Movie.objects.all()
+
+    def list(self, request, *args, **kwargs):
+        queryset = Movie.objects.all()
+        serializer = MovieSerializerFormattedInfo(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = MovieSerializerFormattedInfo(instance)
+        return Response(serializer.data)
